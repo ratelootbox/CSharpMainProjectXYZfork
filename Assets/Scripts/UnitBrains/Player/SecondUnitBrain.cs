de432587d99b2f591cced0426218dc154a877972
Vitalii Diachenkov <v.diachenkov@gmail.com>
2024-01-17 23:29:16 +0200

    HW 3 feedback fixes
    * Use local var for GetTemperature()
    * Deleted unnecessary else close
    * For starts with 0, not 1
    * IncreaseTemperature() now calls after adding projectiles to cannon

--- Assets/Scripts/UnitBrains/Player/SecondUnitBrain.cs
+++ Assets/Scripts/UnitBrains/Player/SecondUnitBrain.cs
@@ -16,22 +16,18 @@ namespace UnitBrains.Player
         protected override void GenerateProjectiles(Vector2Int forTarget, List<BaseProjectile> intoList)
         {
             float overheatTemperature = OverheatTemperature;
-            if (GetTemperature() >= overheatTemperature)
-            {
-                return;
-            }
-            else
-            {
-                IncreaseTemperature();
+            float temp = GetTemperature();
 
-                for (int i = 1; i <= GetTemperature(); i++)
-                {
-                    var projectile = CreateProjectile(forTarget);
-                    AddProjectileToList(projectile, intoList);
-                }
-               
+            if (temp >= overheatTemperature) return;
+          
+            for (int i = 0; i <= temp; i++)
+            {
+                var projectile = CreateProjectile(forTarget);
+                AddProjectileToList(projectile, intoList);
             }
-       
+
+            IncreaseTemperature();
+
         }
 
         public override Vector2Int GetNextStep()
